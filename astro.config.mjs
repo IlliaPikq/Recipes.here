ffe7037d40f5fef8f6b6983355208ac556040177
Illia <illiapikhotskyi@gmail.com>
2023-12-07 21:40:35 +0100

    perfomance optimizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  output: 'server',
+  output: 'hybrid',
   compressHTML: true,
   adapter: vercel()
 });
